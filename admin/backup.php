7059c362f8715a064bd314c45ebd325de504a549
natsumendoza <mendozalaxus@yahoo.com>
2017-03-30 15:24:46 +0800

    updates

--- admin/backup.php
+++ admin/backup.php
@@ -4,106 +4,7 @@
 
  ?>
 
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <title>MySQL database backup</title>
-        <script charset="utf-8" type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function() {
-                $(".restoreFile").hide();
-            });
-            function checkParameters(){
-                var host = $.trim($("#host").val());
-                var username = $.trim($("#username").val());
-                var password = $.trim($("#password").val());
-                var databasename = $.trim($("#databasename").val());
-                if (host == ""){
-                    alert("Plsease enter mysql host.");return false;
-                }
-                else if (username == ""){
-                    alert("Plsease enter mysql username.");return false;
-                }
-                else if (databasename == ""){
-                    alert("Plsease enter mysql database name.");return false;
-                }
-                else if($("#restore").is(':checked')){
-                    var filename = $(".restoreFile").val();
-                    if(filename == ""){
-                        alert("Please choose a file.");return false;
-                    }
-                    else{
-                        var valid_extensions = /(\.db|\.sql)$/;   
-                        if (!valid_extensions.test(filename)){ 
-                            alert('Invalid file format.');return false;
-                        }                   
-                    }
-                }
-                else{
-                    return true;
-                }
-            }
-            function showHide(id){
-                if (id == "backup"){
-                    $(".backupRadio").show();
-                    $(".restoreFile").hide();
-                }
-                else{
-                    $(".backupRadio").hide();
-                    $(".restoreFile").show();
-                }
-            }
-        </script>
-    </head>
-    <body>
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" onsubmit="return checkParameters();" enctype="multipart/form-data">
-		<center><a href="index.php">BACK		</a></center><br/>
-        <table border="0" bgcolor="#C0C0C0" align="center" width="40%">
-            <tr>
-                <td colspan="3" align="center"><b>Please enter the following parameters</b></td>
-            </tr>
-            <tr>
-                <td>MySQL host*</td>
-                <td>:</td>
-                <td><input type="text" id="host" name="host" value="localhost"/></td>
-            </tr>
-            <tr>
-                <td>MySQL username*</td>
-                <td>:</td>
-                <td><input type="text" id="username" name="username" value="root"/></td>
-            </tr>
-            <tr>
-                <td>MySQL password*</td>
-                <td>:</td>
-                <td><input type="text" id="password" name="password" value=""/></td>
-            </tr>
-            <tr>
-                <td>MySQL database name*</td>
-                <td>:</td>
-                <td><input type="text" id="databasename" name="databasename" value="capstone"/></td>
-            </tr>
-            <tr>
-                <td colspan="2">Backup <input type="radio" name="backupRestore" id="backup" value="backup" checked="true" onclick="showHide(this.id);" /></td>
-                <td>
-                    <div class="backupRadio">
-                        <input type="radio" name="data" value="wd" checked="true" />With data 
-                        <input type="radio" name="data" value="wod"/>With out data
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>Restore <input type="radio" name="backupRestore" id="restore" value="restore" onclick="showHide(this.id);" /></td>
-                <td></td>
-                <td><input class="restoreFile" type="file" name="databasefile" /></td>
-            </tr>
-            <tr>
-                <td colspan="3" align="center"><input type="submit" value="Submit"/></td>
-            </tr>
-        </table>
-        </form>
-		
-		<?php
+ <?php
 
 if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['databasename'])) {
         $host = trim($_POST['host']);
@@ -123,6 +24,7 @@ if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['datab
             if ($data == "wd"){
                 //With data
                 exec('mysqldump -u '. $username .' -p'. $password .' '. $databasename .' > '. $outputfilename);
+
             }
             else{
                 //Without data
@@ -161,6 +63,8 @@ if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['datab
 
 function backup_tables($host,$user,$pass,$name,$outputfilename)
 {
+
+
 
     $tables = '*';
 
@@ -241,6 +145,107 @@ function backup_tables($host,$user,$pass,$name,$outputfilename)
 }
 
 //backup_tables($dbhost,$dbuser,$dbpass,$dbname);
-	?>	
+    ?>  
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>MySQL database backup</title>
+        <script charset="utf-8" type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $(".restoreFile").hide();
+            });
+            function checkParameters(){
+                var host = $.trim($("#host").val());
+                var username = $.trim($("#username").val());
+                var password = $.trim($("#password").val());
+                var databasename = $.trim($("#databasename").val());
+                if (host == ""){
+                    alert("Plsease enter mysql host.");return false;
+                }
+                else if (username == ""){
+                    alert("Plsease enter mysql username.");return false;
+                }
+                else if (databasename == ""){
+                    alert("Plsease enter mysql database name.");return false;
+                }
+                else if($("#restore").is(':checked')){
+                    var filename = $(".restoreFile").val();
+                    if(filename == ""){
+                        alert("Please choose a file.");return false;
+                    }
+                    else{
+                        var valid_extensions = /(\.db|\.sql)$/;   
+                        if (!valid_extensions.test(filename)){ 
+                            alert('Invalid file format.');return false;
+                        }                   
+                    }
+                }
+                else{
+                    return true;
+                }
+            }
+            function showHide(id){
+                if (id == "backup"){
+                    $(".backupRadio").show();
+                    $(".restoreFile").hide();
+                }
+                else{
+                    $(".backupRadio").hide();
+                    $(".restoreFile").show();
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" onsubmit="return checkParameters();" enctype="multipart/form-data">
+		<center><a href="index.php">BACK		</a></center><br/>
+        <table border="0" bgcolor="#C0C0C0" align="center" width="40%">
+            <tr>
+                <td colspan="3" align="center"><b>Please enter the following parameters</b></td>
+            </tr>
+            <tr>
+                <td>MySQL host*</td>
+                <td>:</td>
+                <td><input type="password" id="host" name="host" value="localhost"/></td>
+            </tr>
+            <tr>
+                <td>MySQL username*</td>
+                <td>:</td>
+                <td><input type="password" id="username" name="username" value="root"/></td>
+            </tr>
+            <tr>
+                <td>MySQL password*</td>
+                <td>:</td>
+                <td><input type="password" id="password" name="password" value=""/></td>
+            </tr>
+            <tr>
+                <td>MySQL database name*</td>
+                <td>:</td>
+                <td><input type="password" id="databasename" name="databasename" value="capstone"/></td>
+            </tr>
+            <tr>
+                <td colspan="2">Backup <input type="radio" name="backupRestore" id="backup" value="backup" checked="true" onclick="showHide(this.id);" /></td>
+                <td>
+                    <div class="backupRadio">
+                        <input type="radio" name="data" value="wd" checked="true" />With data 
+                        <input type="radio" name="data" value="wod"/>With out data
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>Restore <input type="radio" name="backupRestore" id="restore" value="restore" onclick="showHide(this.id);" /></td>
+                <td></td>
+                <td><input class="restoreFile" type="file" name="databasefile" /></td>
+            </tr>
+            <tr>
+                <td colspan="3" align="center"><input type="submit" value="Submit"/></td>
+            </tr>
+        </table>
+        </form>
+		
+		
     </body>   
 </html>
--- admin/showcontract.php
+++ admin/showcontract.php
@@ -72,6 +72,14 @@ if (!isset($_SESSION["manager"])) {
     </div>
 
     </div>
+
+<!-- Page Specific Plugins -->
+    <script src="js/raphael-min.js"></script>
+    <script src="js/morris-0.4.3.min.js"></script>
+    <script src="js/morris/chart-data-morris.js"></script>
+    <script src="js/tablesorter/jquery.tablesorter.js"></script>
+    <script src="js/tablesorter/tables.js"></script>
+    
     </body>
     </html>
 
